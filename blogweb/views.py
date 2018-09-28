2711042d6b588dad21e3facb00e484f77efe1ce4
木叶知秋 <mnkjxy2016@163.com>
2018-09-28 21:19:38 +0800

    Initial commit

---
+++ blogweb/views.py
@@ -0,0 +1,45 @@
+import datetime
+from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
+from django.db.models import Sum
+from django.core.cache import cache
+
+from read_statistics.utils import get_seven_days_read_data,\
+                 get_today_hot_data, get_yesterday_hot_data
+from blog.models import Blog
+
+   
+def get_seven_days_hot_blogs():
+    today = timezone.now().date()
+    date = today - datetime.timedelta(days=7)
+    blogs = Blog.objects.filter(read_details__date__lt=today, read_details__date__gte=date)\
+                    .values('id', 'title')\
+                    .annotate(read_num_sum=Sum('read_details__read_num'))\
+                    .order_by('-read_num_sum')
+
+    return blogs[:8]
+
+
+def home(request):
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_seven_days_read_data(blog_content_type)
+    
+    # 获取 7 天的缓存数据
+    seven_days_hot_data = cache.get('seven_days_hot_data') 
+    if seven_days_hot_data is None:
+        seven_days_hot_data = get_seven_days_hot_blogs() 
+        cache.set('seven_days_hot_data', seven_days_hot_data, 3600)
+        print('缓存')
+    else:
+        print('使用缓存')
+
+    context = {}
+    context['dates'] = dates
+    context['read_nums'] = read_nums
+    context['today_hot_data'] = get_today_hot_data(blog_content_type)
+    context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
+    context['seven_days_hot_data'] = seven_days_hot_data
+    return render(request, 'home.html', context)
+
+
---
+++ read_statistics/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import get_seven_days_read_data
+
+# Create your views here.
